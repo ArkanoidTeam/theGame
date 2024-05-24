c8e1ea7c022680057b528f4200fa36969d46df3c
Vlad Belousov <belousov7vlad@gmail.com>
2024-05-24 12:12:35 +0300

    feat: сверстаны страницы логина и регистрации

--- packages/client/src/pages/signin/Signin.tsx
+++ packages/client/src/pages/signin/Signin.tsx
@@ -1,19 +1,70 @@
 import { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Button } from '@mui/material'
-
+import {
+  Button,
+  Typography,
+  Container,
+  Grid,
+  TextField,
+  Link,
+} from '@mui/material'
+import './styles.scss'
 const Signin: FC = () => {
   const navigate = useNavigate()
   const handleClick = () => {
     navigate('/') // путь к странице, на которую вы хотите перейти
   }
+
   return (
-    <>
-      <h1>Вход</h1>
-      <Button variant="outlined" size="small" onClick={handleClick}>
-        На главную
-      </Button>
-    </>
+    <Container component="main" maxWidth="xs">
+      <div className="paper">
+        <Typography component="h1" variant="h5">
+          Вход
+        </Typography>
+        <form className="form" noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Пароль"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className="submit">
+            Войти
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="/signup" variant="body2">
+                Еще не зарегистрированы? Регистрация
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   )
 }
 
--- packages/client/src/pages/signup/Signup.tsx
+++ packages/client/src/pages/signup/Signup.tsx
@@ -1,20 +1,103 @@
 import { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Button } from '@mui/material'
-
-const Signup: FC = () => {
+import {
+  Button,
+  CssBaseline,
+  Typography,
+  Container,
+  Grid,
+  FormControlLabel,
+  Checkbox,
+  TextField,
+  Link,
+} from '@mui/material'
+import './styles.scss'
+const Signin: FC = () => {
   const navigate = useNavigate()
   const handleClick = () => {
     navigate('/') // путь к странице, на которую вы хотите перейти
   }
+
   return (
-    <>
-      <h1>Регистрация</h1>
-      <Button variant="outlined" size="small" onClick={handleClick}>
-        На главную
-      </Button>
-    </>
+    <Container component="main" maxWidth="xs">
+      <div className="paper">
+        <Typography component="h1" variant="h5">
+          Регистрация
+        </Typography>
+        <form className="form" noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="Имя"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Фамилия"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Пароль"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="Я не буду хейтить проект и ругаться на разрабов"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className="submit">
+            Зарегистрироваться
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="/signin" variant="body2">
+                Уже зарегистрированы? Войты
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   )
 }
 
-export default Signup
+export default Signin
