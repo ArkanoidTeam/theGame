54f01df4883acc626ddea9460c04d8188da6e94e
Igor Mezentsev <schumm@mail.ru>
2024-05-28 21:20:25 +0500

    Переделал страницу "Регистрация"

--- packages/client/src/pages/signup/Signup.tsx
+++ packages/client/src/pages/signup/Signup.tsx
@@ -1,80 +1,70 @@
-import { FC } from 'react'
-import { Button, Grid, TextField } from '@mui/material'
-import {
-  ButtonsContainer,
-  StyledContainer,
-  StyledForm,
-  StyledWrapper,
-} from './styled'
-import { Typography } from '../../components/Typography'
-const Signup: FC = () => {
+import { Button, Grid, Stack, TextField } from '@mui/material'
+import { Page, PageContent } from '../../components'
+
+const Signup = () => {
   return (
-    <StyledContainer component="main" maxWidth="xs">
-      <StyledWrapper>
-        <Typography component="h1" variant="h2" context="Arkanoid" />
-        <Typography component="h3" variant="h3" context="Регистрация" />
-        <StyledForm>
-          <Grid container spacing={3} justifyContent="center">
-            <Grid item xs={8}>
-              <TextField
-                variant="standard"
-                fullWidth
-                id="email"
-                label="Почта"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                variant="standard"
-                fullWidth
-                id="login"
-                label="Логин"
-                name="login"
-                autoComplete="login"
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                variant="standard"
-                fullWidth
-                name="password"
-                label="Пароль"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                variant="standard"
-                fullWidth
-                name="password-repeat"
-                label="Пароль еще раз"
-                type="password"
-                id="password-repeat"
-              />
-            </Grid>
-          </Grid>
-          <ButtonsContainer>
-            <Button
-              type="submit"
+    <Page justifyContent="space-around">
+      <PageContent title="Arkanoid" subtitle="Регистрация">
+        <Grid container spacing={1} justifyContent="center">
+          <Grid item xs={8}>
+            <TextField
+              variant="standard"
               fullWidth
-              variant="contained"
-              color="primary"
-              className="submit">
-              Зарегистрироваться
+              id="email"
+              label="Почта"
+              name="email"
+              autoComplete="email"
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              variant="standard"
+              fullWidth
+              id="login"
+              label="Логин"
+              name="login"
+              autoComplete="login"
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              variant="standard"
+              fullWidth
+              name="password"
+              label="Пароль"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+          </Grid>
+          <Grid item xs={8}>
+            <TextField
+              variant="standard"
+              fullWidth
+              name="password-repeat"
+              label="Пароль еще раз"
+              type="password"
+              id="password-repeat"
+            />
+          </Grid>
+        </Grid>
+        <Stack spacing={1}>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className="submit">
+            Зарегистрироваться
+          </Button>
+          <Grid container>
+            <Button fullWidth variant="text" href="/signin">
+              Войти
             </Button>
-            <Grid container>
-              <Button fullWidth variant="text" href="/signin">
-                Войти
-              </Button>
-            </Grid>
-          </ButtonsContainer>
-        </StyledForm>
-      </StyledWrapper>
-    </StyledContainer>
+          </Grid>
+        </Stack>
+      </PageContent>
+    </Page>
   )
 }
 
