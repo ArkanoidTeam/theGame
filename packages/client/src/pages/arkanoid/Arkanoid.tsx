d91acc5aacdc1745b6deeb727ccc392002d0e90a
Vlad Belousov <belousov7vlad@gmail.com>
2024-05-30 11:45:24 +0300

    conflict resolved

--- packages/client/src/pages/arkanoid/Arkanoid.tsx
+++ packages/client/src/pages/arkanoid/Arkanoid.tsx
@@ -1,5 +1,6 @@
-import React, { FC, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import { Button } from '@mui/material'
+import { Game } from '../../utils/arkanoid-logic/game'
 import {
   Header,
   Footer,
@@ -55,7 +56,18 @@ const Arkanoid: FC = () => {
     setOpen(false)
     navigate('/')
   }
+  const canvasRef = useRef<HTMLCanvasElement>(null)
 
+  useEffect(() => {
+    const canvas = canvasRef.current as HTMLCanvasElement
+    if (!canvas) {
+      console.error('Canvas element not found')
+      return
+    }
+    const context = canvas.getContext('2d') as CanvasRenderingContext2D
+    const newGame = new Game(canvas, context)
+    newGame.init()
+  }, [])
   return (
     <PageContainer>
       <Header>
@@ -65,6 +77,7 @@ const Arkanoid: FC = () => {
       </Header>
       <MainContent>
         <StyledContainer component="main" maxWidth="md">
+          <canvas width="400" height="500" id="game" ref={canvasRef} />
           <StyledWrapper />
         </StyledContainer>
       </MainContent>
