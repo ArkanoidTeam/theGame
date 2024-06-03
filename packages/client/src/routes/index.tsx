a0a2b5801242fdf80b6fb2909c99694f8e920467
= <vinnikviv@gmail.com>
2024-06-03 22:05:30 +0300

    Устранены конфликты, обновлены роуты

--- packages/client/src/routes/index.tsx
+++ packages/client/src/routes/index.tsx
@@ -7,37 +7,41 @@ import { ProtectedRoute } from '../hooks/protected-route'
 
 const AppRoutes: FC = () => {
   return (
-    <>
-      <main className="main">
-        <ErrorBoundary>
-          <Routes>
-            <Route
-              path={Page.MAIN}
-              element={<ProtectedRoute element={<Pages.Main />} />}
-            />
-            <Route
-              path={Page.ARKANOID}
-              element={<ProtectedRoute element={<Pages.Arkanoid />} />}
-            />
-            <Route path={Page.SIGNIN} element={<Pages.Signin />} />
-            <Route path={Page.SIGNUP} element={<Pages.Signup />} />
-            <Route
-              path={Page.PROFILE}
-              element={<ProtectedRoute element={<Pages.Profile />} />}
-            />
-            <Route
-              path={Page.UNAVAILABLE}
-              element={<ProtectedRoute element={<Pages.Unavailable />} />}
-            />
-            <Route path={Page.NOT_FOUND} element={<Pages.NotFound />} />
-            <Route
-              path={Page.LEADERBOARD}
-              element={<ProtectedRoute element={<Pages.Leaderboard />} />}
-            />
-          </Routes>
-        </ErrorBoundary>
-      </main>
-    </>
+    <ErrorBoundary>
+      <Routes>
+        <Route
+          path={Page.MAIN}
+          element={<ProtectedRoute element={<Pages.Main />} />}
+        />
+        <Route
+          path={Page.ARKANOID}
+          element={<ProtectedRoute element={<Pages.Arkanoid />} />}
+        />
+        <Route path={Page.SIGNIN} element={<Pages.Signin />} />
+        <Route path={Page.SIGNUP} element={<Pages.Signup />} />
+        <Route
+          path={Page.PROFILE}
+          element={<ProtectedRoute element={<Pages.Profile />} />}
+        />
+        <Route
+          path={Page.UNAVAILABLE}
+          element={<ProtectedRoute element={<Pages.Unavailable />} />}
+        />
+        <Route path={Page.NOT_FOUND} element={<Pages.NotFound />} />
+        <Route
+          path={Page.LEADERBOARD}
+          element={<ProtectedRoute element={<Pages.Leaderboard />} />}
+        />
+        <Route
+          path={Page.FORUM}
+          element={<ProtectedRoute element={<Pages.Forum />} />}
+        />
+        <Route
+          path={Page.FORUM_THEME}
+          element={<ProtectedRoute element={<Pages.ForumTheme />} />}
+        />
+      </Routes>
+    </ErrorBoundary>
   )
 }
 
