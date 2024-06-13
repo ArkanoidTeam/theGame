1ee9bcd90fc70be4cb1ef00fbb5ce1258f0800e8
Akopyants <akopyants92@yandex.ru>
2024-06-13 23:37:05 +0300

    Update sw.js

--- packages/client/public/sw.js
+++ packages/client/public/sw.js
@@ -1,72 +1,74 @@
-const CACHE_NAME = 'my-site-cache-v1'
-const URLS = ['/']
+const CACHE_NAME = 'my-site-cache-v1';
+const URLS = [
+  '/',
+  '/App.tsx',
+  '/index.css',
+  '/main.tsx',
+  '/built/style.css',
+  '/built/bundle.js',
+  '/api/',
+  '/app/',
+  '/assets/',
+  '/components/',
+  '/hoc/',
+  '/hooks/',
+  '/pages/',
+  '/routes/',
+  '/store/',
+  '/theme/',
+  '/types/',
+  '/utils/',
+  '/index.html'
+];
 
-this.addEventListener('install', event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches
-      .open(CACHE_NAME)
+    caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Opened cache')
-        return cache.addAll(URLS)
+        console.log('Opened cache');
+        return cache.addAll(URLS);
       })
       .catch(err => {
-        console.log(err)
-        throw err
+        console.log(err);
+        throw err;
       })
-  )
-  console.log('install')
-})
+  );
+  console.log('install');
+});
 
-this.addEventListener('activate', event => {
+self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames
-          .filter(_name => {
-            /* Нужно вернуть true, если хотите удалить этот файл из кеша совсем */
-            return false
-          })
-          .map(name => caches.delete(name))
-      )
+        cacheNames.filter(_name => {
+          /* Return true if you want to remove this cache */
+          return false;
+        }).map(name => caches.delete(name))
+      );
     })
-  )
-  console.log('activate')
-})
+  );
+  console.log('activate');
+});
 
-this.addEventListener('fetch', event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    // Пытаемся найти ответ на такой запрос в кеше
     caches.match(event.request).then(response => {
-      // Если ответ найден, выдаём его
       if (response) {
-        return response
+        return response;
       }
 
-      const fetchRequest = event.request.clone()
-      // В противном случае делаем запрос на сервер
-      return (
-        fetch(fetchRequest)
-          // Можно задавать дополнительные параметры запроса, если ответ вернулся некорректный.
-          .then(response => {
-            // Если что-то пошло не так, выдаём в основной поток результат, но не кладём его в кеш
-            if (
-              !response ||
-              response.status !== 200 ||
-              response.type !== 'basic'
-            ) {
-              return response
-            }
+      const fetchRequest = event.request.clone();
+      return fetch(fetchRequest).then(response => {
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response;
+        }
 
-            const responseToCache = response.clone()
-            // Получаем доступ к кешу по CACHE_NAME
-            caches.open(CACHE_NAME).then(cache => {
-              // Записываем в кеш ответ, используя в качестве ключа запрос
-              cache.put(event.request, responseToCache)
-            })
-            // Отдаём в основной поток ответ
-            return response
-          })
-      )
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseToCache);
+        });
+        return response;
+      });
     })
-  )
-})
+  );
+});
