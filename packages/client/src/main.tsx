59b047615e2071fd23736949ef8726a08de22773
Mezentsev Igor <schumm@mail.ru>
2024-07-03 07:55:59 +0500

    Правки после мержа

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -5,14 +5,13 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import AppRoutes from './routes'
+
 const router = createBrowserRouter(AppRoutes)
 ReactDOM.hydrateRoot(
   document.getElementById('root') as HTMLElement,
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
 
 function startServiceWorker() {
