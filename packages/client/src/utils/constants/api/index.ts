39ca7a1773aaebfc65bb75c4243f597d57b1948a
Vitaliy Vinnik <vinnikviv@gmail.com>
2024-07-14 13:54:46 +0300

    Поправлена константа YANDEX_OAUTH_REDIRECT_URI

--- packages/client/src/utils/constants/api/index.ts
+++ packages/client/src/utils/constants/api/index.ts
@@ -1,12 +1,13 @@
 export const YANDEX_OAUTH_API_URI =
   'https://oauth.yandex.ru/authorize?response_type=code'
-export const YANDEX_OAUTH_REDIRECT_URI = `${window.location.protocol}//${window.location.host}`
+export const YANDEX_OAUTH_REDIRECT_URI = new URL(import.meta.url).origin
 export const BASE_API_URI = 'https://ya-praktikum.tech/api/v2'
 export const APP_API =
   (import.meta.env.SERVER_URI || 'http://localhost') +
   ':' +
   (import.meta.env.SERVER_PORT || '3001') +
   '/api'
+// const url = new URL(import.meta.url).origin
 
 export enum ENDPOINTS {
   SIGN_IN = '/auth/signin',
