343a17fa396935e605cf697930fd2e897acb1b6b
= <vinnikviv@gmail.com>
2024-08-08 12:06:20 +0300

    тест настроек

--- packages/client/src/utils/constants/api/index.ts
+++ packages/client/src/utils/constants/api/index.ts
@@ -4,10 +4,7 @@ export const YANDEX_OAUTH_REDIRECT_URI = new URL(import.meta.url).origin
 export const BASE_API_URI = 'https://ya-praktikum.tech/api/v2'
 export const RESOURCES_LINK = `${BASE_API_URI}/resources`
 export const APP_API =
-  (import.meta.env.VITE_SERVER_URI || 'http://localhost') +
-  ':' +
-  (import.meta.env.VITE_SERVER_PORT || '3001') +
-  '/api'
+  (import.meta.env.VITE_SERVER_URI || 'http://localhost') + '/api'
 export enum ENDPOINTS {
   SIGN_IN = '/auth/signin',
   SIGN_UP = '/auth/signup',
